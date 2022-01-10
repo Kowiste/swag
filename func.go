7c957421d9798147f8345f9da3ad8c405267ca66
Pablo Garcia Vivo <pablo.garcia@techxonn.com>
2022-01-10 08:31:18 +0100

    [add] test swaggo

---
+++ func.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Kowiste/swag/error"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorTest struct {
+	err    error
+	status int
+}
+
+// Test godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} errorTest.ErrorTx
+// @Router /test1/ [get]
+func Test1(c *gin.Context) {
+	res := new(gin.Error)
+	c.JSON(http.StatusOK, res)
+}
+
+// Test2 godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} errorTest.ErrorTx
+// @Router /test2/ [get]
+func Test2(c *gin.Context) {
+	res := new(errorTest.ErrorTx)
+	c.JSON(http.StatusOK, res)
+}
+
+// Test3 godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} ErrorTest
+// @Router /test3/ [get]
+func Test3(c *gin.Context) {
+	res := new(ErrorTest)
+	c.JSON(http.StatusOK, res)
+}
